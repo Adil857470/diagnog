aada751d8058df742a864013998f73a7c95d2e47
Adil <adil.khan@appventurez.com>
2022-04-22 17:53:06 +0530

    login sing up

--- diagnog/user/views.py
+++ diagnog/user/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from user.models import *
 from django.contrib.auth import get_user_model
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.template.loader import render_to_string
-# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-# from django.utils.encoding import force_bytes, force_text
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-# from django.utils import six
-# from django.core.mail import EmailMultiAlternatives
+from django.utils import six
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import render, HttpResponse, Http404, get_object_or_404, redirect
 # Create your views here.
 
@@ -47,25 +47,43 @@ def signUp(request):
                 user.is_active = False
                 user.save()
                 phone = request.POST['phone']
-                Profile.objects.create(username=user, phone=phone)
-                # current_site = get_current_site(request)
-                # mail_subject = 'Activate your Seeker account.'
-                # message = render_to_string('email/email_template.html', {
-                #     'user': user,
-                #     'domain': current_site.domain,
-                #     'uid': urlsafe_base64_encode(force_bytes(user.id)),
-                #     'token': account_activation_token.make_token(user),
-                # })
-                # to_email = email
-                # # send_mail(mail_subject, message, 'youremail', [to_email])
-                # msg = EmailMultiAlternatives(
-                #     mail_subject, message, 'youremail', [to_email])
-                # msg.attach_alternative(message, "text/html")
-                # msg.send()
+                Profile.objects.create(user=user, phone=phone)
+                current_site = get_current_site(request)
+                mail_subject = 'Activate your Diagnog user account.'
+                message = render_to_string('email/email_template.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.id)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = email
+                # send_mail(mail_subject, message, 'youremail', [to_email])
+                msg = EmailMultiAlternatives(
+                    mail_subject, message, 'youremail', [to_email])
+                msg.attach_alternative(message, "text/html")
+                msg.send()
+                print('------IF')
             else:
+                print('------ELSE')
                 return render(request, 'user/signup.html', {"message": "Password not matched."})
+            print('------AFTER-ELSE')
             return render(request, 'user/signin.html')
         else:
             return render(request, 'user/signup.html', {"message": "User already exist."})
 
     return render(request, 'user/signup.html')
+
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
